23488d346e1e914090a57040da3078863d1a957c
FabianVladmir <49292220+FabianVladmir@users.noreply.github.com>
2021-12-17 22:47:06 -0500

    16/12/2021 - Problems Kattis
    Avicious pikeman (easy)
    bank queue
    watering glass

---
+++ semana 12/aula 25/bankqueue.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+struct Person{
+    int money;
+    int time;
+    
+    Person(int m, int w){
+        money = m;
+        time = w;
+    }
+};
+
+bool compareMoney(const Person &a, const Person &b){
+        return a.money >= b.money;
+}
+
+void queuePerson(vector<int> &v, Person c){
+    int i = c.time;
+    
+    while(i >= 0){
+        if(v[i] == 0){
+            v[i] = c.money;
+            return;
+        }  
+        i--; 
+    }
+}
+
+int main(){ 
+
+    int N, T, ci, ti;
+    int sum = 0;
+    int i = 0;
+	
+    scanf("%d %d", &N, &T);    
+   
+    vector<int> bQueue(T);
+    vector<Person> money;
+    
+    for(int i = 0; i < N; i++){
+        scanf("%d %d", &ci, &ti);
+        Person c(ci, ti);
+        
+        money.push_back(c); 
+    }    
+	
+    sort(money.begin(), money.end(), compareMoney);    
+    
+    while(i < money.size()){
+        queuePerson(bQueue, money[i]);
+        i++;
+    }    
+    
+    for(int i = 0; i < bQueue.size(); i++){
+        sum += bQueue[i];
+    }
+    
+    printf("%d", sum); 
+    return 0;
+}
